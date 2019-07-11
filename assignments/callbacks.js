61c63ef6b4711e8d178b4c3a1e76cab255fd3b1d
farjadfazli <farjadfazli@gmail.com>
2019-07-11 19:55:19 -0400

    callbacks and closures

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,24 +27,37 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length-1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let result = x+y;
+  cb(result);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let result = x*y;
+  cb(result);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item))
 }
+
+getLength(items, console.log);
+last(items, console.log);
+sumNums(10,5,console.log);
+multiplyNums(10,5,console.log);
+contains('Gum', items, console.log)
 
 /* STRETCH PROBLEM */
 
@@ -52,4 +65,8 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let dupFree = Array.from(new Set(array));
+  cb(dupFree);
 }
+
+removeDuplicates(items, console.log);
